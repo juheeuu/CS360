d4df896f474d8c22db398659769a94e7edc73bdf
akais <akqq24@gmail.com>
2018-11-22 11:00:45 +0000

    add route, change filenames

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+var router = require('./routes/index')(app);
+
+// const mysql = require('mysql');
+// var dbconfig   = require('./config/database.js');
+// var connection = mysql.createConnection(dbconfig);
+//
+// connection.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected to DB");
+// });
+
+// app.get('/', (req, res) =>{
+//   res.end('Hello DB');
+// })
+
+const server = app.listen(3000, () => {
+  console.log("server is running at ssal.sparcs.org:??:3000")
+})
